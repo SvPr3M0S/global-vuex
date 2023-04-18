7bbbf196bfab3646facf124c4606a56821d886f0
FERNANDOII <jimenezfs@crece.uss.edu.pe>
2023-04-18 11:24:34 -0500

    global-vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,13 +2,24 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+   counter: 1 //Data
   },
   getters: {
+    times2(state) {
+      return state.counter * 2//Computed
+    }
   },
   mutations: {
+    setCounter(state, value) {
+      state.counter = value;//set sync
+    }
   },
   actions: {
+    //set async
   },
   modules: {
+    //subdividir the store
   }
 })
+
+
